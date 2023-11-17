715ce2584ddddfadf65d06a17baf118930762111
Rahmat Yuli Andika <116937270+RhmtAndika12@users.noreply.github.com>
2023-11-18 00:18:11 +0700

    dashboard.py
    revisi

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,7 +9,7 @@ def main():
     st.text('Made by Rahmat Yuli Andika')
 
     # Membaca data dari file CSV
-    file_path = ';/dashboard/all_data.csv'
+    file_path = '/dashboard/all_data.csv'
     bike_sharing = pd.read_csv(file_path)
 
 
