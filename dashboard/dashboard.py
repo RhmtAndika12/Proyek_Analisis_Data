9931722ba335ddc36a9bfd9892581d9429e2c783
Rahmat Yuli Andika <116937270+RhmtAndika12@users.noreply.github.com>
2023-11-17 23:26:50 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,15 +1,13 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-sns.set(style='dark')
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
-    st.title('Dashboard Sederhana')
-    st.text('made by Rahmat Yuli Andika')
+    st.title('Dashboard Interaktif Rahmat Yuli Andika')
 
     # Membaca data dari file CSV
-    file_path = 'all_data.csv'
+    file_path = 'all_data.csv' 
     data = pd.read_csv(file_path)
 
     # Menampilkan opsi visualisasi menggunakan radio button
@@ -26,13 +24,11 @@ def main():
         Jumlah penyewaan sepeda menunjukkan peningkatan pada bulan September dan Juni. Di sisi lain, peningkatan jumlah penyewaan sepeda terjadi sekitar pukul 8 pagi dan sekitar pukul 5 atau 6 sore jika dilihat dari perspektif jam.
 
         """)
-
-        
     elif option == 'Perbandingan Musim dengan Jumlah Sewa Sepeda':
         plot_perbandingan_musim(data)
         st.markdown("""
         ### Conclusion
-    
+        
         **Adakah perbedaan dalam frekuensi sewa sepeda harian antara hari kerja (workingday) dan hari libur (holiday)?**
         Ada peningkatan jumlah penyewaan sepeda pada hari kerja dibandingkan dengan hari libur.
         """)
@@ -43,15 +39,18 @@ def plot_histogram(data):
     sns.histplot(data['cnt_daily'], bins=30, kde=True)
     plt.xlabel('Jumlah Sewa Sepeda Harian')
     plt.ylabel('Frekuensi')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
 
 def plot_perbandingan_musim(data):
     st.subheader('Perbandingan Musim dengan Jumlah Sewa Sepeda')
     # Visualisasi menggunakan boxplot untuk perbandingan musim dengan seaborn
-    sns.boxplot(x="season_daily", y="cnt_daily", data=data)
-    plt.xlabel('Musim')
-    plt.ylabel('Jumlah Sewa Sepeda Harian')
-    st.pyplot()
+    fig, ax = plt.subplots()
+    sns.boxplot(x="season_daily", y="cnt_daily", data=data, ax=ax)
+    ax.set_xlabel('Musim')
+    ax.set_ylabel('Jumlah Sewa Sepeda Harian')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
